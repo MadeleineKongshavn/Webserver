d09134ff08f7130d4e318b8c2387ecb194927516
MadeleineKongshavn <Madekoro@hotmail.com>
2015-04-27 02:21:51 +0200

    bandquery

--- WebApplication1/WebApplication1/Managers/BandManager.cs
+++ WebApplication1/WebApplication1/Managers/BandManager.cs
@@ -20,11 +20,11 @@ namespace WebApplication1.Managers
         private String SERVER_API_KEY = "AIzaSyDMdRA7ma1FxaL82Ev3OU8kX2YXIw44ImA";
 
 
-/*        public async Task<List<BandClass>> FindBandBasedOnQuery(String query, int uid)
+        public async Task<List<BandClass>> FindBandBasedOnQuery(String query)
         {
             var db = new DbBand();
-            return await db.FindBandBasedOnQuery(query, uid);
-        }*/
+            return await db.FindBandBasedOnQuery(query);
+        }
         public async Task<List<BandsImagesClass>> GetRandomBands(int userId)
         {
             var db = new DbBand();
