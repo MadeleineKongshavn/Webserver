6e0394d7c1134afe23507a1a55a7b826923dce80
grimveig <rannveigalette@gmail.com>
2015-05-20 11:38:16 +0200

    friendsController webapi cleanup

--- WebApplication1/WebApplication1/Controllers/Api/FriendsController.cs
+++ WebApplication1/WebApplication1/Controllers/Api/FriendsController.cs
@@ -13,26 +13,9 @@ namespace WebApplication1.Controllers.Api
 {
     public class FriendsController : ApiController
     {
-        [HttpPost]
-        [Route("api/Friends/AddFriendRequest/{userId},{friendId}")]
-        public async Task<Boolean> AddFriendRequest(int userId, int friendId)
-        {
-            using (var mngr = ManagerFactory.GetFriendManager())
-            {
-                return await mngr.AddFriendRequest(userId, friendId);
-            }
-        }
-        [HttpPost]
-        [Route("api/Friends/SetFriendAccept/{id},{ok}")]
-        public async Task<Boolean> SetFriendAccept(int id, Boolean ok)
-        {
-            using (var mngr = ManagerFactory.GetFriendManager())
-            {
-                return await mngr.SetFriendAccept(id, ok);
-            }
-        }
+        //api/Friends/FriendsToUser/{id} ||endret || sjekk
         [HttpGet]
-        [Route("api/Friends/FriendsToUser/{id}")]
+        [Route("api/Friends/GetFriends/{userid}")]
         public async Task<List<FriendsClass>> FriendsToUser(int id)
         {
             using (var mngr = ManagerFactory.GetFriendManager())
@@ -41,17 +24,9 @@ namespace WebApplication1.Controllers.Api
             }
         }
 
-        [HttpPost]
-        [Route("api/Friends/CancelFriendTask/{uid},{friendId}")]
-        public async Task<bool> CancelFriendTask(int uid, int friendId)
-        {
-            using (var mngr = ManagerFactory.GetFriendManager())
-            {
-                return await mngr.CancelFriendTask(friendId, uid);
-            }
-        }
+        //api/Friends/FindFriend/{name},{uid} || endret || sjekk
         [HttpGet]
-        [Route("api/Friends/FindFriend/{name},{uid}")]
+        [Route("api/Friends/Query/{name},{userid}")]
         public async Task<FriendsClass> FindFriend(String name, int uid)
         {
             using (var mngr = ManagerFactory.GetFriendManager())
@@ -59,6 +34,42 @@ namespace WebApplication1.Controllers.Api
                 return await mngr.FindFriend(name, uid);
             }
         }
+
+
+
+        //api/Friends/AddFriendRequest/{userId},{friendId} || endret
+        [HttpPost]
+        [Route("api/Friends/SendRequest/{userId},{friendId}")]
+        public async Task<Boolean> AddFriendRequest(int userId, int friendId)
+        {
+            using (var mngr = ManagerFactory.GetFriendManager())
+            {
+                return await mngr.AddFriendRequest(userId, friendId);
+            }
+        }
+
+        //api/Friends/SetFriendAccept/{id},{ok} ||endret || sjekk
+        [HttpPost]
+        [Route("api/Friends/Respond/{requestId},{ok}")]
+        public async Task<Boolean> SetFriendAccept(int id, Boolean ok)
+        {
+            using (var mngr = ManagerFactory.GetFriendManager())
+            {
+                return await mngr.SetFriendAccept(id, ok);
+            }
+        }
+
+        //api/Friends/CancelFriendTask/{uid},{friendId} || endret || sjekk
+        [HttpPost]
+        [Route("api/Friends/Remove/{userid},{friendId}")]
+        public async Task<bool> CancelFriendTask(int uid, int friendId)
+        {
+            using (var mngr = ManagerFactory.GetFriendManager())
+            {
+                return await mngr.CancelFriendTask(friendId, uid);
+            }
+        }
+
 
     }
 }
